b24d6e95225370c8d1f73af21f9e5c52e21527a0
kamilzhvn <kamilzhvn@gmail.com>
2019-07-18 12:08:55 +0600

    Add files via upload

---
+++ Advanced Core Data/Advanced Core Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Advanced Core Data
+//
+//  Created by macbook on 7/4/19.
+//  Copyright © 2019 macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
